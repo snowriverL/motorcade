c3e28e5f0ca3ef33ac9039f447f4be6ccdc5908c
m18210540611@163.com <gfh261022>
2019-12-12 15:10:25 +0800

    优惠券活动配置修改

--- motorcade-activity/src/main/java/com/dorcen/activity/config/BaseMetaObjectHandler.java
+++ motorcade-activity/src/main/java/com/dorcen/activity/config/BaseMetaObjectHandler.java
@@ -38,29 +38,17 @@ public class BaseMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         // 创建人ID
-        Object createBy = getFieldValByName(CREATE_BY, metaObject);
-        if (createBy == null) {
-            setFieldValByName(CREATE_BY, 0, metaObject);
-        }
-
+        setFieldValByName(CREATE_BY, 0, metaObject);
         // 创建时间
-        Object createDate = getFieldValByName(CREATE_DATE, metaObject);
-        if (createDate == null) {
-            setFieldValByName(CREATE_DATE, new Date(), metaObject);
-        }
-
+        setFieldValByName(CREATE_DATE, new Date(), metaObject);
         // 删除标记
-        Object delFlag = getFieldValByName(DEL_FLAG, metaObject);
-        if (delFlag == null) {
-            setFieldValByName(DEL_FLAG, 0, metaObject);
-        }
+        setFieldValByName(DEL_FLAG, 0, metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         // 填充更新人的ID
         setFieldValByName(UPDATE_BY, 0, metaObject);
-
         // 填充更新时间
         setFieldValByName(UPDATE_DATE, new Date(), metaObject);
     }
